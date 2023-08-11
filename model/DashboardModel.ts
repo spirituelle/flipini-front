1518cf6589895fad9ec26ae10e57ef0a7fac82df
spirituelle <adnanerouhi@gmail.com>
2023-08-11 04:38:21 +0100

    fix MagasinDetailsModel import

--- model/DashboardModel.ts
+++ model/DashboardModel.ts
@@ -1,9 +1,9 @@
 import {CardModel} from './CardModel'
-import {MagasinModel} from './MagasinDetailsModel'
+import {SubCategoryModel} from './SubCategoryModel'
 
 export interface DashboardModel{
     status:String,
     cards: CardModel[],
-    top_magasin: MagasinModel[],
+    top_magasin: SubCategoryModel[],
 
 }
