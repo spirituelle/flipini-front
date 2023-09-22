ee07a28d752276fb599f7f2cd9988e8e13174837
spirituelle <adnanerouhi@gmail.com>
2023-09-22 22:36:46 +0100

    with axos

--- app/(site)/magasins/component/moreMagasin.tsx
+++ app/(site)/magasins/component/moreMagasin.tsx
@@ -3,6 +3,7 @@ import  Link  from 'next/link';
 import  Image  from 'next/image';
 import {useState} from 'react';
 
+import axios from './../../../../lib/axios'
 import {SubCategoryModel} from './../../../../model/SubCategoryModel';
 // import { getMagasin } from './../actions'
 
@@ -11,14 +12,35 @@ import FavoriteIcon from './../../../../components/FavoriteIcon'
 async function getMagasin( {api_token, page = 1,search}:{api_token: string, page?: number, search?: string | undefined}){
     let user= {id: null};
         if(api_token){
-            const userRes = await fetch(
-                `${process.env.BACKEND_URL}/api/user`, { headers: { Authorization: "Bearer " + api_token} }
-            );
-             user= await userRes.json();
+            const userRes = await axios.get('/api/user',  { headers: { Authorization: "Bearer " + api_token } })
+            console.log(userRes)
+            user= userRes.data;
+            // .then(response => { 
+            //   dispatch({ type: "LOGIN" , user:{...response.data, api_token: state.token}})
+            //     // setUser(response.data); 
+            //     // setError(response.data.data.error); 
+            //     // return response.data.data
+            // })
+            // .catch(err => {
+            //     console.log(err.response);
+            //     // setToken(null)
+            //     deleteCookie('api_token');
+            //     dispatch({ type: "LOGOUT", user: {} as UserModel })
+      
+            //     // setUser({} as UserModel)
+            // })
+
+            // const userRes = await fetch(
+            //     `${process.env.NEXT_PUBLIC_STORAGE_END_POINT}/api/user`, { headers: { Authorization: "Bearer " + api_token} }
+            // );
+            //  user= await userRes.json();
         }
-    const res = await fetch(`${process.env.NEXT_PUBLIC_STORAGE_END_POINT}/api/magasin-list?user_id=${user.id}&country_id=1&page=${page}&per_page=24&search=${search}`, { cache: 'no-store'});
-    const data = await res.json();
-    return data?.data as SubCategoryModel[];
+        const res = await axios.get(`/api/magasin-list?user_id=${user.id}&country_id=1&page=${page}&per_page=24&search=${search}`  )
+        console.log(res)
+        return res.data.data as SubCategoryModel[];
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_STORAGE_END_POINT}/api/magasin-list?user_id=${user.id}&country_id=1&page=${page}&per_page=24&search=${search}`, { cache: 'no-store'});
+    // const data = await res.json();
+    // return data?.data as SubCategoryModel[];
 }
 
 export default function MAgasinsWithMore({initialShop, search, api_token}: {initialShop: SubCategoryModel[], search: string | undefined, api_token: string}){
